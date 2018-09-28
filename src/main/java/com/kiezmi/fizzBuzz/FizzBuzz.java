70328d43108b897a0d27f484bca995c9def23815
Enrique de Miguel <Kiezmi@gmail.com>
2018-09-28 12:24:31 +0200

    Creacion del test

---
+++ src/main/java/com/kiezmi/fizzBuzz/FizzBuzz.java
@@ -0,0 +1,5 @@
+package com.kiezmi.fizzBuzz;
+
+public class FizzBuzz {
+
+}
---
+++ src/test/java/com/kiezmi/fizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package com.kiezmi.fizzBuzz;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void firstTest(){
+        assertTrue(true);
+    }
+
+
+}
