803bf644599419edbcafffa05a769ee90d602296
Enrique de Miguel <kiezmi@gmail.com>
2018-09-28 12:53:37 +0200

    [BLUE]Actualizacion del test de 3

--- src/main/java/com/kiezmi/fizzBuzz/FizzBuzz.java
+++ src/main/java/com/kiezmi/fizzBuzz/FizzBuzz.java
@@ -1,7 +1,13 @@
 package com.kiezmi.fizzBuzz;
 
 public class FizzBuzz {
+
     public boolean isFizz(int value) {
         return 0 == value % 3;
     }
+
+    public boolean isBuzz(int value) {
+        return 0 == value % 5;
+    }
+
 }
--- src/test/java/com/kiezmi/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/com/kiezmi/fizzBuzz/FizzBuzzTest.java
@@ -1,10 +1,29 @@
 package com.kiezmi.fizzBuzz;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
+@RunWith(DataProviderRunner.class)
+
 public class FizzBuzzTest {
+
+    @DataProvider
+    public static Object[][] dataNumberProvider(){
+        return new Object [][] {
+                {3,true},
+                {6,true},
+                {9,true},
+                {12,true},
+                {15,true},
+                {18,true},
+        };
+    }
 
     @Test
     public void firstTest() {
@@ -12,14 +31,15 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void itShoulReturnFizzifDivisiblebythree() {
+    @UseDataProvider("dataNumberProvider")
+    public void itShoulReturnFizzifDivisiblebythree(final int input,final boolean spected) {
         //Arrange
         FizzBuzz fizzbuzz = new FizzBuzz();
         int value = 3;
         //Action
-        boolean result = fizzbuzz.isFizz(value);
+        boolean result = fizzbuzz.isFizz(input);
         //Assertion
-        assertTrue(result);
+        assertEquals(spected,result);
     }
     public void itShoulReturnFizzifDivisiblebyfive() {
         //Arrange
