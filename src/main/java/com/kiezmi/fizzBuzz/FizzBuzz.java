e5ecfbaa8a92ef5ba2c3f15b54996207123d5499
Enrique de Miguel <Kiezmi@gmail.com>
2018-09-28 13:23:28 +0200

    [BLUE]Refactor magic numbers

--- src/main/java/com/kiezmi/fizzBuzz/FizzBuzz.java
+++ src/main/java/com/kiezmi/fizzBuzz/FizzBuzz.java
@@ -2,12 +2,15 @@ package com.kiezmi.fizzBuzz;
 
 public class FizzBuzz {
 
+    public static final int FIZZ_FACTOR = 3;
+    public static final int BUZZ_FACTOR = 5;
+
     public boolean isFizz(int value) {
-        return 0 == value % 3;
+        return 0 == value % FIZZ_FACTOR;
     }
 
     public boolean isBuzz(int value) {
-        return 0 == value % 5;
+        return 0 == value % BUZZ_FACTOR;
     }
 
     public boolean isFizzBuzz(int value) {
